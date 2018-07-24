291d943e5278d47fec438359e61e09df05ca294f
Jorge Acetozi <jorge.acetozi@gmail.com>
2018-07-24 17:33:06 -0300

    Update formula version 0.10.0

--- Formula/test-cli.rb
+++ Formula/test-cli.rb
@@ -1,8 +1,8 @@
 class TestCli < Formula
   desc "Simple test CLI in Go."
   homepage "https://jorgeacetozi.com/"
-  url "https://github.com/jorgeacetozi/test-cli/releases/download/0.8.0/test-cli_0.8.0_Darwin_x86_64.tar.gz"
-  version "0.8.0"
+  url "https://github.com/jorgeacetozi/test-cli/releases/download/0.10.0/test-cli_0.10.0_Darwin_x86_64.tar.gz"
+  version "0.10.0"
   sha256 "b0dc1a9611723b65b0172a73081bfc02423136612867e616b59c0caec0d288f3"
 
   def install
