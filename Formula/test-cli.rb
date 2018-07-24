75552af79e9d8950ac505bdaa0b10d0596e74d02
Jorge Acetozi <jorge.acetozi@gmail.com>
2018-07-24 17:15:27 -0300

    Add homebrew formula

---
+++ Formula/test-cli.rb
@@ -0,0 +1,15 @@
+class TestCli < Formula
+  desc "Simple test CLI in Go."
+  homepage "https://jorgeacetozi.com/"
+  url "https://github.com/jorgeacetozi/test-cli/releases/download/0.7.0/test-cli_0.7.0_Darwin_x86_64.tar.gz"
+  version "0.7.0"
+  sha256 "b0dc1a9611723b65b0172a73081bfc02423136612867e616b59c0caec0d288f3"
+
+  def install
+    bin.install "test-cli"
+  end
+
+  test do
+    system "#{bin}/program version"
+  end
+end
